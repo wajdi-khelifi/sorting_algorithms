09381b7b1ffe01ec0aab0b57c97c5b1ac372ebe2
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2023-11-20 15:02:21 +0000

    Header file

--- sort.h
+++ sort.h
@@ -40,4 +40,8 @@ int lomuto(int *array, ssize_t first, ssize_t last, size_t size);
 void quick_s(int *array, ssize_t first, ssize_t last, int size);
 void quick_sort(int *array, size_t size);
 
+/* Task 4 : Shell sort - Knuth Sequence */
+void shell_sort(int *array, size_t size);
+void swap(int *array, int elem1, int elem2);
+
 #endif
